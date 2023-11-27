0e8313a0cddf28394901b325dc50a577795f0fbf
2joaopedro <joao.pedro.franchini.4@gmail.com>
2023-11-27 13:45:17 -0300

    Adicionado diretório 'modules' contendo controlador e entidade para gestão de candidatos. Implementado método POST para inserção de dados.

---
+++ src/main/java/br/com/joaopedrofranchini/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,20 @@
+package br.com.joaopedrofranchini.gestaovagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joaopedrofranchini.gestaovagas.modules.candidate.CandidateEntity;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+    
+    
+    @PostMapping("/")
+    // Este método acessa a requisição, colocando as informações contidas no corpo (body) da solicitação na instância da entidade CandidateEntity.
+    public void createCandidate(@RequestBody CandidateEntity candidateEntity) {
+            System.out.println("Candidato ".concat(candidateEntity.getUsername()));
+        }
+}
